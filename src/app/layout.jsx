4c768723d3f09dfeb0f701a0cc61bee2dfdede02
Rachid Laasri <rashidlaasri@gmail.com>
2023-11-29 23:38:05 +0100

    init

---
+++ src/app/layout.jsx
@@ -0,0 +1,46 @@
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+import clsx from 'clsx'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+// Use local version of Lexend so that we can use OpenType features
+const lexend = localFont({
+  src: '../fonts/lexend.woff2',
+  display: 'swap',
+  variable: '--font-lexend',
+})
+
+export const metadata = {
+  title: {
+    template: '%s - Laravel Web Installer',
+    default: 'Laravel Web Installer - Documentation',
+  },
+  description:
+    'Streamline Laravel deployment with a user-friendly web installer - Documentation',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={clsx('h-full antialiased', inter.variable, lexend.variable)}
+      suppressHydrationWarning
+    >
+      <body className="flex min-h-full bg-white dark:bg-slate-900">
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
+      </body>
+    </html>
+  )
+}
---
+++ src/lib/navigation.js
@@ -0,0 +1,25 @@
+export const navigation = [
+    {
+        title: 'Installation',
+        links: [
+            { title: 'Getting started', href: '/' },
+            { title: 'Requirements', href: '/docs/requirements' },
+            { title: 'Installation', href: '/docs/installation' },
+        ],
+    },
+    {
+        title: 'Configurations',
+        links: [
+            { title: 'Adding steps', href: '/docs/adding-steps' },
+            { title: 'Ordering steps', href: '/docs/ordering-steps' },
+            { title: 'Routes prefix', href: '/docs/routes-prefix', },
+            { title: 'Redirection', href: '/docs/redirection' },
+            { title: 'Middleware', href: '/docs/middleware', },
+            { title: 'Installer requirements', href: '/docs/installer-requirements' },
+            { title: 'Installer permissions', href: '/docs/installer-permissions' },
+            { title: 'Migrations and seeding', href: '/docs/migrations-and-seeding' },
+            { title: 'Environnement variables', href: '/docs/environnement-variables' },
+            { title: 'Events and listeners', href: '/docs/events-and-listeners' },
+        ],
+    },
+]
